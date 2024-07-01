9b765950e7ba318b0c2c763565d3b8d10fb8fad5
Nikeshh <nikeshhbaskaran01@gmail.com>
2024-07-01 11:32:02 -0400

    Admin layout changes

--- client/src/layouts/admin.tsx
+++ client/src/layouts/admin.tsx
@@ -1,35 +1,38 @@
 import { Outlet } from "react-router";
 import { useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { ModeToggle } from "@/components/global/mode-toggle";
 import Sidebar from "./_components/sidebar";
+import AdminSearch from "./_components/admin-search";
+import MobileSidebar from "./_components/mobile-sidebar";
 
 const AdminLayout = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/admin/login");
+    }
+  }, [navigate]);
 
-    useEffect(() => {
-      const token = localStorage.getItem('token');
-      if (!token) {
-        navigate('/admin/login');
-      }
-    }, [navigate]);
-    
-	return (
-		<>
-            <div className="h-full">
-                <header className="z-50 flex items-center gap-4 border-b bg-muted/40 h-[64px] px-4 md:pl-[19rem] fixed inset-y-0 w-full">
-                    <div className="w-full flex-1">
-                    </div>
-                    <ModeToggle />
-                </header>
-                <Sidebar />
-                <main className="mx-4 md:pl-72 h-full pt-[80px]">
-                    <Outlet />
-                </main>
-            </div>
-		</>
-	);
+  return (
+    <>
+      <div className="h-full">
+        <header className="z-50 flex items-center gap-4 border-b bg-muted/40 h-[64px] px-4 md:pl-[19rem] fixed inset-y-0 w-full">
+          <MobileSidebar />
+          <div className="w-full flex-1">
+            <AdminSearch />
+          </div>
+          <ModeToggle />
+        </header>
+        <Sidebar />
+        <main className="mx-4 md:pl-72 h-full pt-[80px]">
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default AdminLayout;
